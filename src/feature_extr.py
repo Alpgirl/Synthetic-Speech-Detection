5d2324d632dac8c4073d35fbea2db695feea28a1
hernannenjer <159898739+hernannenjer@users.noreply.github.com>
2024-05-23 09:57:39 +0300

    Update feature_extr.py
    add the features

--- src/feature_extr.py
+++ src/feature_extr.py
@@ -1,10 +1,29 @@
 import torch
+from librosa import util
+import librosa.feature
+import numpy as np
 
-def logpowerspec():
-    pass
 
-def MFCC():
-    pass
 
-def CQCC():
-    pass
+# def logpowerspec():
+#     pass
+
+# def MFCC():
+#     pass
+
+# def CQCC():
+#     pass
+
+
+def get_MFCC(waveform, sample_rate):
+    mfcc = librosa.feature.mfcc(y=waveform.numpy().squeeze(), sr=sample_rate, n_mfcc=20)
+    return torch.from_numpy(mfcc)
+
+def get_CQCC(waveform, sample_rate):
+    cqcc = librosa.feature.chroma_cqt(y=waveform.numpy().squeeze(), sr=sample_rate)
+    return torch.from_numpy(cqcc)
+
+def get_LPS(waveform, sample_rate):
+    stft = librosa.core.stft(waveform.numpy().squeeze())
+    lps = librosa.amplitude_to_db(np.abs(stft))
+    return torch.from_numpy(lps)
